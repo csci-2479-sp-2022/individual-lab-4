29c809bb8f73f5219883516d2f6509d266006c34
Mihaela Valac <mvalac@student.cscc.edu>
2022-03-17 15:49:10 -0400

    Created -mfsc for VidoeGames, System, and Category. Added data for each table in the seed files.

---
+++ app/Http/Controllers/VideoGameController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\VideoGame;
+
+class VideoGameController extends Controller
+{
+    public function show()
+    {
+        return view('video-games', [
+            'games' => self::getGames(),
+        ]);
+    }
+
+    private static function getGames()
+    {
+        return VideoGame::with(['system', 'categories'])->get();
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VideoGameController;
+use App\Models\VideoGame;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/video-games', [VideoGameController::class, 'show']);
