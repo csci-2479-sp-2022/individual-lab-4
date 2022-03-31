f866a6bd92cc4675f604bc27d1920a08e9bdc8f5
Harrison Xu <hxu2@student.cscc.edu>
2022-03-31 18:49:05 -0400

    Lab 4 Completed

---
+++ database/migrations/2022_03_30_213000_create_video_games_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video_games', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->year('release_year');
+            $table->boolean('completed')->default(false);
+            $table->foreignId('system_id')->constrained();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('video_games');
+    }
+};
